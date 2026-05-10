c8120df69fb35d804c733cacea02e609b2af1250
Chashe Amora <hanzoh612@gmail.com>
2026-05-10 13:50:53 +0800

    update

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RFID Access System</title>
+    <title>RFID Access System i like <menu></menu></title>
     <style>
         * {
             margin: 0;
