dbd1c1cd01fdba9daddbe252e8e2d31c95e4e251
Chashe Amora <hanzoh612@gmail.com>
2026-05-10 13:37:38 +0800

    Access

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RFID Control System</title>
+    <title>RFID Access Control System</title>
     <style>
         * {
             margin: 0;
