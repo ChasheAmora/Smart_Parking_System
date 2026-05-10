d3db8430a42669f3f0c08ae581a8163968865d90
Chashe Amora <hanzoh612@gmail.com>
2026-05-10 13:44:41 +0800

    Auto-commit:

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RFID Access Control System</title>
+    <title>RFID</title>
     <style>
         * {
             margin: 0;
